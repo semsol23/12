3b9870af0be8be27865204c0ba92f20264811060
semsol23 <semsol23@naver.com>
2020-11-19 16:36:18 +0900

    7

--- main.c
+++ main.c
@@ -7,17 +7,31 @@ int main(int argc, char *argv[]) {
 	
 	FILE *fp;
 	char str[100];
+	char path[100];
+	char lookup[100];
 	
-	fp = fopen("sample.txt", "r");
+	printf("input file path : ");
+	scanf("%s", path);
+	printf("input search word : ");
+	scanf("%s", lookup);
+	
+	fp = fopen(path, "r");
 	if (fp == NULL)
 	{
+		printf("invalid path! (%s)\n", path);
 		return -1;
 	}
 	
 	while (fgets(str, 100, fp) != NULL)
 	{
-		printf("%s", str);
+		if (strncmp(str, lookup, strlen(lookup)) == 0)
+		{
+			printf("Search succeed!\n");
+			fclose(fp);
+			return 0;
+		}
 	}
+	printf("search failed!\n");
 	
 	fclose(fp);
 	
